7d57f1d37e38b9cd48b578fe90f3ca73b803cd7a
ADPer <adper0705@gmail.com>
2024-05-12 23:53:10 +0530

    created a list of options containing the three options, and used the random library to set the computer to randomly choose among the three choices

--- RockPaperScissor/rps_main.py
+++ RockPaperScissor/rps_main.py
@@ -1,8 +1,10 @@
+import random
+
 def get_choices():
+    options = ["rock", "paper", "scissors"]
+
     player_choice = input("Input a choice (rock, paper, scissor) : ")
-    computer_choice = "paper"
+    computer_choice = random.choice(options)
     choices = {"player" : player_choice, "computer" : computer_choice}
     return choices
 
-choices = get_choices()
-print(choices)
