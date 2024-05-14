313be6e0627421eff1ccd836b8c7e87050ec6531
ADPer <adper0705@gmail.com>
2024-05-15 03:29:07 +0530

    completed the hand class
    added the functionalities of calculating values, getting the value or accessing the value and displaying the hands while keeping in consideration whether it is the dealer or the player

--- BlackJack/main.py
+++ BlackJack/main.py
@@ -55,18 +55,51 @@ class Deck:
         return cards_delt
     
 class Hand:
+    # initializing an empty hand
     def __init__(self, dealer = False):
         self.cards = []
         self.value = 0
         self.dealer = dealer
 
+    # adding cards to the hand
     def add_card(self, card_list):
-            self.cards.extend(card_list)
+        self.cards.extend(card_list)
 
+    # calculating the value of tha hand
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
 
-deck = Deck()
-deck.shuffle()
+        for card in self.cards:
+            card_value = int(card.rank["value"])
+            self.value += card_value
+            if card.rank["rank"] == "A":
+                has_ace = True
+        # determining whether ace should be 11 or 1
+        if has_ace and self.value > 21:
+            self.value -= 10
 
-hand = Hand()
-hand.add_card(deck.deal(2))
-print(hand.cards)
+    # getting the value of a hand
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+    
+    # Checking for black jack win
+    def is_blackjack(self):
+        return self.get_value() == 21
+    
+    # Displaying hand
+    def display(self, show_all_dealer_cards=False):
+        print(f''' {"Dealer's" if self.dealer else "Your" }  hand : ''')
+        for index, card in enumerate(self.cards):
+            if index == 0 and \
+                self.dealer and \
+                    not show_all_dealer_cards and \
+                        not self.is_blackjack():
+                print("Hidden")
+            else:
+                print(card)
+
+        if not self.dealer:
+            print("Value : ", self.get_value())
+        print()
